faf05618387525b8861dcae0ee0e656484c1b067
Pedro Henrique Vinhas <pedrohenrique.hrzon@gmail.com>
2024-07-10 12:38:14 +0000

    feat(bolsa-valores/src/orders/orders.service): implement create order method.

---
+++ apps/bolsa-valores/src/orders/orders.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Order } from './entities/order.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class OrdersService {
+  constructor(
+    @InjectModel(Order.name)
+    private orderModel: Model<Order>
+  ) {}
+
+  create(createOrderDto: CreateOrderDto) {
+    return this.orderModel.create({
+      ...createOrderDto,
+      status: 'PENDING',
+    })
+  }
+
+  findAll() {
+    return `This action returns all orders`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} order`;
+  }
+
+  update(id: number, updateOrderDto: UpdateOrderDto) {
+    return `This action updates a #${id} order`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} order`;
+  }
+}
